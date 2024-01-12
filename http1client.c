08cf084e98920433b9c379428a4d65d4a14a7fe0
Behnejad <3690943+behnejad@users.noreply.github.com>
2024-01-12 10:06:24 +0330

    Update http1client.c

--- http1client.c
+++ http1client.c
@@ -1,3 +1,10 @@
+/*
+	Creative Commons Attribution-NonCommercial-NoDerivs (CC-BY-NC-ND)
+	https://creativecommons.org/licenses/by-nc-nd/4.0/
+	The most restrictive creative commons license.
+	This only allows people to download and share your work for no commercial gain and for no other purposes.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
